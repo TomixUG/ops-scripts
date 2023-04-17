16b8a6a877792c6d248f3791c242815dfc343961
TomixUG <tomas.sousek1@gmail.com>
2023-04-18 01:54:39 +0200

    add 6 and gn

---
+++ 6/DiskImageBackup.bat
@@ -0,0 +1,49 @@
+@echo off
+
+:: prvni zkontrolujeme jestli ma uzivatel administratorske prava, pokud ne ukoncime program
+:: udelame to zavolanim prikazu, ke kteremu maji pristup pouze admins
+dism >nul 2>&1
+if errorlevel 1 (
+    echo ERROR: Script musi byt spusten jako admin
+    exit /b 1
+) 
+
+echo ==== Zalohovani disku =====
+echo varovani: nelze zalohovat aktualne pouzivany disk
+echo.
+
+:: snazime se od uzivatele dostat vstup, ptame se do te doby nez ho da
+set partition=
+set /p partition="Zadejte partition, kterou chcete zalohovat (napr. D:\): "
+:loop
+if "%partition%"=="" (
+    set /p partition="Zadejte partition, kterou chcete zalohovat (napr. D:\): "
+    goto loop
+)
+
+:: snazime se od uzivatele dostat vstup, ptame se do te doby nez ho da
+set imagename=
+set /p imagename="Zadejte cestu kam chcete ulozit bitovou kopii (napr. C:\backup.wim): "
+:loop2
+if "%imagename%"=="" (
+    set /p imagename="Zadejte cestu kam chcete ulozit bitovou kopii (napr. C:\backup.wim): "
+    goto loop2
+)
+
+echo.
+echo Jdeme zalohovat partition %partition%
+echo Bitova kopie se ulozi do %imagename%
+echo.
+echo == Log programu DISM: ==
+
+:: udelame bitovou kopii a compresujeme ji
+dism /Capture-Image /ImageFile:%imagename% /CaptureDir:%partition% /Name:MyImage /Compress:Max /CheckIntegrity
+
+echo.
+
+if errorlevel 1 (
+    echo == ERROR: Nastal problem v prubehu vytvareni bitove kopie! ==
+) else (
+    echo == Disk byl uspesne zalohovan ==
+)
+
