b39c2dcdb505dc90f35185b4a2609e4a759d162b
TomixUG <tomas.sousek1@gmail.com>
2023-04-24 23:36:23 +0200

    1: add more messages

--- 1/systeminfo_.bat
+++ 1/systeminfo_.bat
@@ -1,27 +1,34 @@
 @echo off
 
 echo Ukladani vsech potrebnych informaci do systeminfo.txt...
+echo.
 
 rem program vypíše aktuální systémové informace, na začátku přepíše soubor
+echo Ukladani datum...
 echo Aktualni datum %DATE% %TIME% > systeminfo.txt
 
 rem vypise nazev prihlaseneho uzivatele
 echo. >> systeminfo.txt
+echo Ukladani nazvu prihlaseneho uzivatele...
 echo Nazev prihlaseneho uzivatele: %USERNAME% >> systeminfo.txt
 
 rem vypise systemove info
+echo Ukladani vystupu z prikazu systeminfo...
 echo. >> systeminfo.txt
 echo Systeminfo: >> systeminfo.txt
 systeminfo >> systeminfo.txt >nul 2>&1
 
 rem vypise paths
+echo Ukladani paths...
 echo. >> systeminfo.txt
 echo Vyhledavaci cesty (path): >> systeminfo.txt
 echo %PATH% >> systeminfo.txt
 
 rem vypise systemove promenne
+echo Ukladani systemovych promennych...
 echo. >> systeminfo.txt
 echo Systemove promenne: >> systeminfo.txt
 set >> systeminfo.txt
 
+echo.
 echo Hotovo!
